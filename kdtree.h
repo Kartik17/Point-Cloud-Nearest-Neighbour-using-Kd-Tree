5f1c887288c46c184ce1196375586e9ad5239524
Kartik <kartiksah7@gmail.com>
2021-03-28 19:19:18 -0400

    Update kdtree.h

--- kdtree.h
+++ kdtree.h
@@ -21,8 +21,7 @@ class Kdtree{
             this->num_pts = 0;
         }
 
-        Kdtree(PointCloud<T>& pc, size_t dim){
-            this->total_dim = dim;
+        Kdtree(PointCloud<T>& pc, const size_t& d): dim(d){
             this->num_pts = pc.get_size();
             this->root = this->make_tree(pc,0, this->num_pts, 0);
         }
