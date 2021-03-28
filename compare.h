50a9365e50c67fac9398cc0a3a99bb64acaa0f4f
Kartik <kartiksah7@gmail.com>
2021-03-28 18:54:29 -0400

    Update compare.h

--- compare.h
+++ compare.h
@@ -6,11 +6,11 @@ template <class T>
 struct Comparator{
     size_t sort_dim;
     Comparator(const size_t& d){
-        sort_dim = d;
+        this->sort_dim = d;
     }
     
     bool operator ()(const Point<T>& p1, const Point<T>& p2){
-        return (p1.get_dim(sort_dim) < p2.get_dim(sort_dim));
+        return (p1.get_dim(this->sort_dim) < p2.get_dim(this->sort_dim));
     }
 };
 
